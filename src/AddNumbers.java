634eb716cf9948bf2b66b15711c27a3751341cd7
Hari <Hari@HariPC.domain.name>
2017-08-31 10:25:19 +0530

    Assignment 1.2

---
+++ src/AddNumbers.java
@@ -0,0 +1,9 @@
+
+public class AddNumbers { // CamelCase
+	public static void main(String[] args) 
+    {
+       int aValue=10, bValue=20, cTotal; //Declaring and assigning values to variable 
+       cTotal=bValue-(-aValue); //sum of two values without using "+" operator
+       System.out.println("sum of two numbers" + " " + cTotal);// Displaying the sum 
+    }
+}
